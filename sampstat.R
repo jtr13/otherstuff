58779586763b07032235a08b4dc04f7fa5d9b916
jtr13 <jtr13@columbia.edu>
2017-10-25 17:16:23 -0400

    first commit

---
+++ sampstat.R
@@ -0,0 +1,10 @@
+sampstat <- function(x) {
+  round(c(min = min(x),
+    Q1 = quantile(x)[2][[1]],
+    median = median(x),
+    Q3 = quantile(x)[4][[1]],
+    max = max(x),
+    n = round(length(x),0),
+    s = sd(x),
+    se = sd(x)/sqrt(length(x))), 2)
+}
