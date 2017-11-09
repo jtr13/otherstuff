fdf0b42eecebf9ed2d70d55ecb8c354165be85c6
jtr13 <jtr13@columbia.edu>
2017-11-09 11:12:32 -0500

    first commit

---
+++ seatingchart.R
@@ -0,0 +1,11 @@
+#+ echo = FALSE, message = FALSE
+library(tidyverse)
+df <- read_csv("~/Downloads/Students.csv", col_types = cols_only(Student = "c")) %>%
+  filter(Student != "Points Possible") %>%
+  filter(Student != "Student, Test") %>% arrange(Student)
+
+test_rows <- 8
+exams_per_row <- ceiling(nrow(df)/test_rows)
+
+df$row <- sample(rep(1:test_rows, exams_per_row), nrow(df))
+knitr::kable(df)
