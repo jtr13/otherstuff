63b706447cb55289ae309d7e8bbc2a52dc762c86
jtr13 <jtr13@columbia.edu>
2017-12-16 10:18:36 -0500

    first commit

---
+++ answerrect.R
@@ -0,0 +1,9 @@
+# answerrect.R
+# use fig.height = 2
+smallrect <- function() {
+  par(mar = c(0,0,0,0))
+  plot(1, 2, xlim = c(0,8), ylim = c(0, 2),
+       col = "white", ann = FALSE,
+       axes = FALSE)
+  rect(6, 0, 8, .5)
+}
