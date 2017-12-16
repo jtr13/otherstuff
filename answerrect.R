350865658d3f05efe46816803d890c6ef072a591
jtr13 <jtr13@columbia.edu>
2017-12-16 15:42:15 -0500

    added largerect()

--- answerrect.R
+++ answerrect.R
@@ -7,3 +7,11 @@ smallrect <- function() {
        axes = FALSE)
   rect(6, 0, 8, .5)
 }
+
+largerect <- function() {
+  par(mar = c(0,0,0,0))
+  plot(1, 2, xlim = c(0,8), ylim = c(0, 2),
+       col = "white", ann = FALSE,
+       axes = FALSE)
+  rect(4, 0, 8, .5)
+}
