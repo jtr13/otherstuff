2dd124ee1bd6c23c2f2a451ca3a3e064f9510602
jtr13 <jtr13@columbia.edu>
2017-12-16 09:54:27 -0500

    add comments, remove "details = FALSE"

--- countpoints.R
+++ countpoints.R
@@ -1,8 +1,13 @@
+# bracket must be in the same position in each line
+# Ex.
+# 1.  [3 points]
+# 14. [1 points]
+#
 library(dplyr)
-countpoints <- function(filename = "Stat1201Test2Sample.Rmd", details = FALSE) {
+countpoints <- function(filename = "Stat1201Test2Sample.Rmd") {
   x <- readLines(filename)
   y <- grep("[1-9] points|10 points", x, value = TRUE)
-  bracket <- stringr::str_locate(y[1], "\\[")[1]
+  bracket <- stringr::str_locate(y[1], "\\[")[1] # find position of bracket in the first line
   z <- sapply(y, function(x) substr(x, bracket + 1, bracket + 2)) %>%
     as.numeric()
   print(rbind(1:length(z), z))
