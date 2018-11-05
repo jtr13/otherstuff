d1b483fd8eda7cb02ee7b3b8e021c153337f2201
Joyce Robbins <jtr13@columbia.edu>
2018-11-05 16:29:32 -0500

    small changes

--- seatingchart.R
+++ seatingchart.R
@@ -1,4 +1,5 @@
 #+ echo = FALSE, message = FALSE
+# Export student list from CourseWorks Grades
 library(tidyverse)
 df <- read_csv("~/Downloads/Students.csv", col_types = cols_only(Student = "c")) %>%
   filter(Student != "Points Possible") %>%
@@ -8,5 +9,8 @@ test_rows <- 8
 exams_per_row <- ceiling(nrow(df)/test_rows)
 
 df$row <- sample(rep(1:test_rows, exams_per_row), nrow(df))
-print("Seating Chart")
 knitr::kable(df)
+
+
+
+
