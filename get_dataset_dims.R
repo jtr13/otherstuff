cf34f99d8317a58b8d0b05d8ae79320a7d33ccd8
Joyce Robbins <jtr13@columbia.edu>
2019-09-07 14:15:18 -0400

    deal with spaces in names and non data frames

--- get_dataset_dims.R
+++ get_dataset_dims.R
@@ -1,7 +1,16 @@
 get_dataset_dims <- function(packagename = NULL) {
   datasetnames <- data(package = packagename)$results[,3]
-  nrow <- purrr::map_int(datasetnames, ~dim(get(.x))[1])
-  ncol <- purrr::map_int(datasetnames, ~dim(get(.x))[2])
-  data.frame(name = datasetnames, nrow, ncol)
+
+  # get rid of everything after space in dataset name
+  datasetnames <- unlist(purrr::map(strsplit(datasetnames, " "), ~.x[[1]]))
+
+  nrow <- purrr::map(datasetnames, ~dim(get(.x))[1])
+  ncol <- purrr::map(datasetnames, ~dim(get(.x))[2])
+
+  # change NULLs to NAs
+  nrow <- unlist(purrr::map(nrow, ~ifelse(is.null(.x), NA, .x)))
+  ncol <- unlist(purrr::map(ncol, ~ifelse(is.null(.x), NA, .x)))
+  firstclass <- unlist(purrr::map(datasetnames, ~class(get(.x))[1]))
+  data.frame(name = datasetnames, nrow, ncol, firstclass = firstclass)
 }
 
