7594fe74b6646adcd9406d75c38e8945ac4b6cf4
Joyce Robbins <jtr13@columbia.edu>
2019-09-07 16:16:14 -0400

    added library call, package name

--- get_dataset_dims.R
+++ get_dataset_dims.R
@@ -1,6 +1,10 @@
 get_dataset_dims <- function(packagename = NULL) {
   datasetnames <- data(package = packagename)$results[,3]
 
+  datasetpackages <- data(package = packagename)$results[,1]
+
+  if (!is.null(packagename)) library(packagename, character.only = TRUE)
+
   # get rid of everything after space in dataset name
   datasetnames <- unlist(purrr::map(strsplit(datasetnames, " "), ~.x[[1]]))
 
@@ -11,6 +15,6 @@ get_dataset_dims <- function(packagename = NULL) {
   nrow <- unlist(purrr::map(nrow, ~ifelse(is.null(.x), NA, .x)))
   ncol <- unlist(purrr::map(ncol, ~ifelse(is.null(.x), NA, .x)))
   firstclass <- unlist(purrr::map(datasetnames, ~class(get(.x))[1]))
-  data.frame(name = datasetnames, nrow, ncol, firstclass = firstclass)
+  allclasses <- tibble::enframe(purrr::map(datasetnames, ~class(get(.x))), name = NULL, value = "allclasses")
+  cbind(data.frame(name = datasetnames, package = datasetpackages, nrow, ncol, firstclass), allclasses)
 }
-
