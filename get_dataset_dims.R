25b003adaf934df3a3be9cee9d7fb9200d60ea7f
Joyce Robbins <jtr13@columbia.edu>
2019-09-07 18:09:50 -0400

    add data type columns

--- get_dataset_dims.R
+++ get_dataset_dims.R
@@ -1,3 +1,9 @@
+fnum <- function(dataset) ifelse("data.frame" %in% class(get(dataset)), summary(as.factor(unlist(lapply(get(dataset), class))))["numeric"], NA)
+
+fchar <- function(dataset) ifelse("data.frame" %in% class(get(dataset)), summary(as.factor(unlist(lapply(get(dataset), class))))["character"], NA)
+
+ffac <- function(dataset) ifelse("data.frame" %in% class(get(dataset)), summary(as.factor(unlist(lapply(get(dataset), class))))["factor"], NA)
+
 get_dataset_dims <- function(packagename = NULL) {
   datasetnames <- data(package = packagename)$results[,3]
 
@@ -16,5 +22,10 @@ get_dataset_dims <- function(packagename = NULL) {
   ncol <- unlist(purrr::map(ncol, ~ifelse(is.null(.x), NA, .x)))
   firstclass <- unlist(purrr::map(datasetnames, ~class(get(.x))[1]))
   allclasses <- tibble::enframe(purrr::map(datasetnames, ~class(get(.x))), name = NULL, value = "allclasses")
-  cbind(data.frame(name = datasetnames, package = datasetpackages, nrow, ncol, firstclass), allclasses)
+
+  numericols <- unlist(purrr::map(datasetnames, fnum))
+  charcols <- unlist(purrr::map(datasetnames, fchar))
+  facols <- unlist(purrr::map(datasetnames, ffac))
+
+  cbind(data.frame(name = datasetnames, package = datasetpackages, nrow, ncol, firstclass, numericols, charcols, facols), allclasses)
 }
