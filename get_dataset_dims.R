e550c15c08827d7ea5b8669698c93a5f2dd56b64
Joyce Robbins <jtr13@columbia.edu>
2019-09-14 14:16:52 -0400

    many improvements

--- get_dataset_dims.R
+++ get_dataset_dims.R
@@ -1,8 +1,24 @@
-fnum <- function(dataset) ifelse("data.frame" %in% class(get(dataset)), summary(as.factor(unlist(lapply(get(dataset), class))))["numeric"], NA)
+# provides information on built in datasets in R
 
-fchar <- function(dataset) ifelse("data.frame" %in% class(get(dataset)), summary(as.factor(unlist(lapply(get(dataset), class))))["character"], NA)
+get_type_count <- function(dataset, datatype) {
+  if("data.frame" %in% class(get(dataset))) {
+    num_cols <- summary(as.factor(unlist(lapply(get(dataset), class))))[datatype]
+    if (is.na(num_cols)) num_cols <- 0
+  } else {
+    num_cols <- NA
+  }
+  num_cols
+}
 
-ffac <- function(dataset) ifelse("data.frame" %in% class(get(dataset)), summary(as.factor(unlist(lapply(get(dataset), class))))["factor"], NA)
+get_nrow <- function(name) {
+  nrow <- ifelse(sum(c("data.frame", "matrix") %in% class(get(name))) > 0,
+                 dim(get(name))[[1]], NA)
+}
+
+get_ncol <- function(name) {
+  nrow <- ifelse(sum(c("data.frame", "matrix") %in% class(get(name))) > 0,
+                 dim(get(name))[[2]], NA)
+}
 
 get_dataset_dims <- function(packagename = NULL) {
   datasetnames <- data(package = packagename)$results[,3]
@@ -14,18 +30,38 @@ get_dataset_dims <- function(packagename = NULL) {
   # get rid of everything after space in dataset name
   datasetnames <- unlist(purrr::map(strsplit(datasetnames, " "), ~.x[[1]]))
 
-  nrow <- purrr::map(datasetnames, ~dim(get(.x))[1])
-  ncol <- purrr::map(datasetnames, ~dim(get(.x))[2])
+#  nrow <- unlist(purrr::map(datasetnames, get_nrow))
 
-  # change NULLs to NAs
-  nrow <- unlist(purrr::map(nrow, ~ifelse(is.null(.x), NA, .x)))
-  ncol <- unlist(purrr::map(ncol, ~ifelse(is.null(.x), NA, .x)))
-  firstclass <- unlist(purrr::map(datasetnames, ~class(get(.x))[1]))
-  allclasses <- tibble::enframe(purrr::map(datasetnames, ~class(get(.x))), name = NULL, value = "allclasses")
+#  ncol <- unlist(purrr::map(datasetnames, get_ncol))
 
-  numericols <- unlist(purrr::map(datasetnames, fnum))
-  charcols <- unlist(purrr::map(datasetnames, fchar))
-  facols <- unlist(purrr::map(datasetnames, ffac))
+  dim <- purrr::map_chr(datasetnames,
+                        ~ifelse(length(dim(get(.x))) > 0,
+                                paste(dim(get(.x)), collapse = "  "),
+                                NA))
 
-  cbind(data.frame(name = datasetnames, package = datasetpackages, nrow, ncol, firstclass, numericols, charcols, facols), allclasses)
+  ncol <- unlist(purrr::map(datasetnames,
+                            ~ifelse(length(dim(get(.x))) == 2,
+                                    dim(get(.x))[[2]],
+                                    NA)))
+
+  length <- unlist(purrr::map(datasetnames, ~ifelse(is.null(dim(get(.x))), length(get(.x)), NA)))
+
+  first_class_listed <- unlist(purrr::map(datasetnames, ~class(get(.x))[1]))
+
+  allclasses <- tibble::enframe(purrr::map(datasetnames, ~class(get(.x))),
+                                name = NULL, value = "allclasses")
+
+  numeric_cols <- unlist(purrr::map2(datasetnames, "numeric", get_type_count))
+  integer_cols <- unlist(purrr::map2(datasetnames, "integer", get_type_count))
+  factor_cols <- unlist(purrr::map2(datasetnames, "factor", get_type_count))
+  character_cols <- unlist(purrr::map2(datasetnames, "character", get_type_count))
+  date_cols <- unlist(purrr::map2(datasetnames, "Date", get_type_count))
+
+  other_cols <- ncol - (numeric_cols + integer_cols + factor_cols +
+                          character_cols + date_cols)
+
+  cbind(data.frame(package = datasetpackages, name = datasetnames, dim,
+                   length, first_class_listed, numeric_cols, integer_cols, factor_cols,
+                   character_cols, date_cols, other_cols), allclasses)
 }
+
